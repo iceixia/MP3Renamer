ed5c9d001295c0f819b998170a01bb637bb77156
iceixia <androidftw1994@gmail.com>
2017-11-28 14:55:09 +0000

    initial commit

--- MP3Renamer/Program.cs
+++ MP3Renamer/Program.cs
@@ -1,15 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace MP3Renamer
 {
-    class Program
+    public class Root
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                args = new string[1];
+                args[0] = "blank";
+            }
+            
+            if (args[0] == "h" || args[0] == "help")
+            {
+                HelpText();
+            }
+
+            string TargetFolder = "";
+
+            if (Directory.Exists(args[0]))
+            {
+                TargetFolder = args[0];
+            }
+            else
+            {
+                TargetFolder = Directory.GetCurrentDirectory();
+            }
+
+            string[] FilesList = Directory.GetFiles(TargetFolder);
+
+            int NumberOfFiles = FilesList.Length;
+            int CurrentFile = 1;
+            foreach (string entry in FilesList)
+            {
+                Console.Write("[" + CurrentFile.ToString() + @"/" + NumberOfFiles.ToString() + "] ");
+
+                if (!entry.EndsWith(".mp3"))
+                {
+                    Console.WriteLine("Skipping - not mp3 file");
+                }
+                else
+                {
+                    TagLib.File CurrentTrack = TagLib.File.Create(entry);
+
+                    string Artist = CurrentTrack.Tag.FirstPerformer;
+                    string Title = CurrentTrack.Tag.Title;
+
+                    if (Artist == null || Artist == "" || Title == "" || Title == null)
+                    {
+                        Console.WriteLine("Skipping - incomplete id3 tags");
+                    }
+                    else
+                    {
+                        string NewFilename = Artist + " - " + Title + ".mp3";
+                        string CurrentFilename = Path.GetFileName(entry);
+                        string NewPath = entry.Replace(CurrentFilename, NewFilename);
+
+                        File.Move(entry, NewPath);
+
+                        Console.WriteLine("File Renamed");
+                    }
+                }
+
+                CurrentFile++;
+            }
+
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+        }
+
+        public static void HelpText()
+        {
+            Console.WriteLine("Usage: MP3Renamer <path>");
+            Console.WriteLine("Note the path is optional, we'll just look in the current directory otherwise");
+            Environment.Exit(0);
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+## MP3 Renamer
+
+So you've converted your music collection off of youtube sent it all through something like picard and the ID3 tags are all properly set, but alas the file names contain things like (official video), (some other bullshit youtubers like to add in).
+
+Well now this tool solves all your problems, it sets the file names of all of your mp3's to <artist> - <title>.mp3.
+
+### Usage
+
+MP3Renamer.exe <path (optional)>
+
+Notice: if you don't specify a path we'll assume you wan to run mp3renamer on the current directory.
